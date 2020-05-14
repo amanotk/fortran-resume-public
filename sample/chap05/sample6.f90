a7e937fe0a9b1126c6158dceba2bdc0705e7526d
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2020-05-14 13:54:32 +0900

    update

--- sample/chap05/sample6.f90
+++ sample/chap05/sample6.f90
@@ -10,8 +10,8 @@ program sample
   ! 配列のメモリが連続した要素に順に読み込まれる
   read(*,*) x
 
-  do j = 1, m
-     do i = 1, n
+  do j = 1, 4
+     do i = 1, 3
         write(*,*) i, j, x(i,j)
      end do
   end do
